127c79c6cbaad2677954f14dae9de8983b1f10dd
FelipeGXavier <felipexavier20015@gmail.com>
2022-03-22 22:20:17 -0300

    WIP Geolocalization

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const grpc = require('@grpc/grpc-js');
 const path = require('path');
+const process = require('process');
 const protoLoader = require('@grpc/proto-loader');
 const PROTO_PATH = path.resolve('./proto/localization.proto');
 const packageDefinition = protoLoader.loadSync(
@@ -17,6 +18,9 @@ const grpService = grpc.loadPackageDefinition(packageDefinition).localization;
 
 const client = new grpService.Localization('localhost:50051', grpc.credentials.createInsecure());
 
-client.GetAddress({latitude: "0", longitude: "0"}, function(err, response) {
+const latitude = process.argv[2];
+const longitude = process.argv[3];
+
+client.GetAddress({latitude, longitude}, function(err, response) {
     console.log(response);
 });
